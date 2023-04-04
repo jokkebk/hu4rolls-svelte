8765fd20a909ce23e3bac08f2937bae251d9f91f
Joonas Pihlajamaa <joonas.pihlajamaa@gmail.com>
2023-04-05 00:06:12 +0300

    First working version

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,39 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+    let wsAddr = '';
+    let ws = null;
+    let cmd = '';
+    
+    function connectWs() {
+        if (ws) {
+            ws.close();
+        }
+        ws = new WebSocket(wsAddr);
+        ws.onopen = () => {
+            console.log('ws open');
+        };
+        ws.onmessage = (msg) => {
+            console.log('ws message', msg);
+        };
+        ws.onclose = () => {
+            console.log('ws close');
+        };
+    }
+    
+    function sendCmd() {
+        if (ws) {
+            ws.send(cmd);
+        }
+    }
+</script>
+
+<h1>Hu4Rolls-2 Svelte Client</h1>
+
+<p>
+    <input bind:value={wsAddr} placeholder="Paste WS address here">
+    <button on:click={connectWs}>Connect</button>
+</p>
+
+<p>
+    <input bind:value={cmd} placeholder="Enter command here">
+    <button on:click={sendCmd}>Send command</button>
+</p>
